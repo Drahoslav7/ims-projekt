6695470d70db35975310655ce2601a787d6c409e
Drahoslav Bednář <drahoslav@yo2.cz>
2015-12-08 17:32:03 +0100

    basics

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,83 @@
-#include "simlib/simlib.h"
+/**
+ * IMS
+ * David Balvín (xbalvi00)
+ * Drahoslav Bednář (xbedna55)
+ */
 
+#include "simlib/simlib.h"
+#include <string>
+#include <vector>
+#include <algorithm>
+#include "schedule.h"
+
+using namespace std;
+
+enum TrainType : int {
+	TYPE_START,
+	TYPE_PASS,
+	TYPE_END,
+};
+
+
+class Train : public Process {
+public:
+	TrainType type;
+
+	Train(TrainType t){
+		type = t;
+	}
+
+	void Behavior(){
+
+	}
+};
+
+
+class Station : public Facility {
+public:
+	int numberOftracks;
+
+	Station(const char* name, int n) : Facility(name) {
+		numberOftracks = n;
+	}
+
+};
+
+
+class Generator : public Event {
+public:
+	vector<ScheduleRecord> schedule;
+	int scheduleIndex;
+	TrainType type;
+
+	Generator(vector<ScheduleRecord> records, TrainType type) {
+		schedule = records;
+		sort(schedule.begin(), schedule.end());
+		scheduleIndex = 0;
+		this->type = type;
+	}
+
+	void Behavior(){
+		(new Train(type))->Activate();
+		if(scheduleIndex >= schedule.size()){
+			return;
+		}
+		Activate(schedule[scheduleIndex].time);
+		scheduleIndex++;
+	}
+};
+
+// GLOBALS:
+
+Generator startG(scheduleStart, TYPE_START);
+Generator passG(schedulePass, TYPE_PASS);
+Generator endG(scheduleEnd, TYPE_END);
+
+
+/** Hlavní nádraží **/
+Station mainStation("main station", 10);
+
+/** MAIN **/
 int main(int argc, char const *argv[])
 {
 	
---
+++ src/schedule.h
@@ -0,0 +1,63 @@
+
+#define TIME(h,m) (60*(h)+(m))
+
+typedef double t_time;
+
+struct ScheduleRecord
+{
+	t_time time;
+	int line;
+
+	friend bool operator<(const ScheduleRecord& x, const ScheduleRecord& y){
+		return (x.time < y.time);
+	}
+};
+
+
+const std::vector<ScheduleRecord> scheduleEnd = {
+	// TIME(),
+};
+
+
+
+const std::vector<ScheduleRecord> scheduleStart = {
+	// 340:
+	{TIME(0,35), 340},
+	{TIME(4,25), 340},
+	{TIME(5,12), 340},
+	{TIME(6,11), 340},
+	{TIME(7,35), 340},
+	{TIME(8,12), 340},
+	{TIME(9,28), 340},
+	{TIME(10,15), 340},
+	{TIME(11,28), 340},
+	{TIME(12,15), 340},
+	{TIME(12,49), 340},
+	{TIME(13,28), 340},
+	{TIME(13,48), 340},
+	{TIME(14,14), 340},
+	{TIME(14,47), 340},
+	{TIME(15,28), 340},
+	{TIME(15,48), 340},
+	{TIME(16,14), 340},
+	{TIME(16,47), 340},
+	{TIME(17,28), 340},
+	{TIME(17,48), 340},
+	{TIME(18,15), 340},
+	{TIME(18,48), 340},
+	{TIME(19,28), 340},
+	{TIME(20,15), 340},
+	{TIME(21,28), 340},
+	{TIME(22,48), 340},
+
+	
+};
+
+const std::vector<ScheduleRecord> schedulePass = {
+
+	// TIME(),	
+	// TIME(),
+	// TIME(),
+	// TIME(),
+
+};
